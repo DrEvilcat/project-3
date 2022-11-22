e8006c64da2b437137fb29127441f45ec77266ba
LiamBaker <liamjbaker@gmail.com>
2022-11-22 19:02:21 +1100

    Adding API functionality to the Flask API

--- app.py
+++ app.py
@@ -1,6 +1,25 @@
-import sqlite3
+#import sqlite3
+import numpy as np
+import pandas as pd
 import random
-from flask import Flask, session, render_template, request, g
+from flask import Flask, session, render_template, request
+from sqlalchemy import create_engine
+import datetime
+import config
+
+# connect to SQL database 
+
+protocol = 'postgresql'
+username = config.username
+password = config.password
+host = 'localhost'
+port = 5432
+database_name = config.database_name
+rds_connection_string = f'{protocol}://{username}:{password}@{host}:{port}/{database_name}'
+engine = create_engine(rds_connection_string)
+conn = engine.connect()
+
+
 
 app = Flask(__name__)
 
@@ -8,17 +27,21 @@ app = Flask(__name__)
 def index():
     return "<h1>This is my test App</h1>"
 
-def get_db():
-    db = getattr(g, '_database', None)
-    if db is None:
-        db = g._database = sqlite3.connect('weatherobs')
-    return db
 
-@app.teardown_appcontext
-def close_connection(exception):
-    db = getattr(g, '_database', None)
-    if db is not None:
-        db.close()
+@app.route("/api/v1.0/stationdata")
+def stationdata():
+    #Return JSON of station data table
+    return pd.read_sql("SELECT * FROM station_data", con=conn).to_json()
 
-if __name__ == '__main__':
-    app.run()
+@app.route("/api/v1.0/fcst")
+def fcst():
+    return pd.read_sql("SELECT * FROM fcst", con=conn).to_json()
+    #Return fcst data tables
+    return 
+@app.route("/api/v1.0/obs")
+def obs():
+    #Return obs data tables
+    return pd.read_sql("SELECT * FROM obs", con=conn).to_json() 
+
+if __name__ == "__main__":
+    app.run(debug=True)
