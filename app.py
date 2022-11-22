11f2911a17ae5cf118c76abcffec79f6bbf7d9cf
John Antony <johnantony2994@gmail.com>
2022-11-22 20:26:01 +1100

    app.py issue John

--- app.py
+++ app.py
@@ -6,6 +6,11 @@ from flask import Flask, session, render_template, request
 from sqlalchemy import create_engine
 import datetime
 import config
+from splinter import Browser
+from bs4 import BeautifulSoup as bs
+import time
+from webdriver_manager.chrome import ChromeDriverManager
+import requests
 
 # connect to SQL database 
 
@@ -43,5 +48,22 @@ def obs():
     #Return obs data tables
     return pd.read_sql("SELECT * FROM obs", con=conn).to_json() 
 
-if __name__ == "__main__":
-    app.run(debug=True)
+@app.route("/api/v1.0/weatherweekly")
+def weatherweekly():
+     # Set up Splinter
+    executable_path = {'executable_path': ChromeDriverManager().install()}
+    browser = Browser('chrome', **executable_path, headless=False)
+
+    # Visit visitcostarica.herokuapp.com
+    url = "http://www.bom.gov.au/"
+    browser.visit(url)
+
+    # Scrape page into Soup
+    html = browser.html
+    soup = bs(html, "html.parser")
+
+
+
+
+    if __name__ == "__main__":
+        app.run(debug=True)
